dad165264a799d9ec3200fc234950e0e18119674
Tanajah Rosenbaum <tanajahr25@gmail.com>
2020-04-05 01:51:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def take_a_number(katz_deli, person)
      puts "Welcome, #{person}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving(katz_deli)
+def now_serving(katz_deli, person)
  if katz_deli.length == 0 
    puts "There is nobody waiting to be served!"
  elsif
