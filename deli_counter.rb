364850273b38516fe40566c5e9a612b95b398440
Tanajah Rosenbaum <tanajahr25@gmail.com>
2020-04-05 01:21:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli == 0 
+    puts "The line is currently empty."
+  else katz_deli > 2
+  current_line = "The line is currently:"
+  person = []
+    katz_deli.each.with_index(1) do |person, index|
+      current_line << "The line is currently: #{index}. #{person}"
+end
+return current_line
+end
+ 
